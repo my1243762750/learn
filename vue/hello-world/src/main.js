f937e671be9b210cdb8be33fc6e4def26c5af4cb
my1243762750 <1243762750@qq.com>
2021-01-19 21:49:12 +0800

    去除无关文件

--- vue/hello-world/src/main.js
+++ vue/hello-world/src/main.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
-import VueAMap from 'vue-amap';
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 
 Vue.use(ElementUI)
-Vue.use(VueAMap);
-VueAMap.initAMapApiLoader({
-    key: 'your amap key',
-    plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
-    // 默认高德 sdk 版本为 1.4.4
-    v: '1.4.4'
-});
 Vue.config.productionTip = false
 
 new Vue({
