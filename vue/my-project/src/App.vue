8fe30011d46d3d612d974605b8baf539fc8d8395
jamei <jamei@deloitte.com.cn>
2021-02-20 10:47:22 +0800

    1谷歌插件

--- vue/my-project/src/App.vue
+++ vue/my-project/src/App.vue
@@ -6,18 +6,71 @@
       <my-table-column prop="name" label="姓名" width="100"></my-table-column>
       <my-table-column prop="address" label="地址" width="100"></my-table-column>
     </my-table>
+    <!--后备内容-->
+    <submit-button></submit-button>
+    <submit-button>Save</submit-button>
+    <!--2.6.0 v-slot-->
+    <layout>
+      <template v-slot:header>
+        <div>header</div>
+      </template>
+      <!--v-slot可以缩写为#-->
+      <template #default>
+        <div>body</div>
+      </template>
+      <!--这种方式取代 slot 和 scope-slot-->
+      <!--动态插槽 暂时不演示-->
+      <template v-slot:footer="{msg, age}">
+        <div>footer {{ msg }} {{ age }}</div>
+      </template>
+    </layout>
+    <br>
+    <!--2.6.0 v-slot-->
+    <layout v-slot="obj">
+      {{ obj }}
+    </layout>
+    <br>
+    <!--2.6.0以前 废弃的语法 slot-->
+    <layout>
+      <template slot="header">
+        <div>header</div>
+      </template>
+      <template>
+        <div>body</div>
+      </template>
+      <template slot="footer">
+        <div>footer</div>
+      </template>
+    </layout>
+    <br>
+    <!--2.6.0以前 废弃的语法 slot-scope-->
+    <layout>
+      <template slot="header" slot-scope="obj">
+        <div>header {{ obj }}</div>
+      </template>
+      <template>
+        <div>body</div>
+      </template>
+      <template slot="footer">
+        <div>footer</div>
+      </template>
+    </layout>
   </div>
 </template>
 
 <script>
 import MyTable from "./components/MyTable";
 import MyTableColumn from "./components/MyTableColumn";
+import SubmitButton from "./components/Button";
+import Layout from "./components/Layout";
 
 export default {
   name: 'App',
   components: {
     MyTable,
-    MyTableColumn
+    MyTableColumn,
+    SubmitButton,
+    Layout
   },
   data() {
     return {
