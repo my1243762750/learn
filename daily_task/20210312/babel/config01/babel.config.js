356616643a40fd2d9d9e9d6f53a41b88c95b1dd4
jamei <jamei@deloitte.com.cn>
2021-03-12 15:13:39 +0800

    1测试babel

---
+++ daily_task/20210312/babel/config01/babel.config.js
@@ -0,0 +1,16 @@
+/*module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: []
+    }
+}*/
+
+module.exports = {
+    presets: [
+        '@babel/preset-env'
+    ],
+    plugins: []
+}
---
+++ daily_task/20210312/babel/config01/src/js/index.js
@@ -0,0 +1,4 @@
+const fn = () => {
+    console.log('hello world')
+}
+const result = Promise.resolve(1);
