f478bd2885c94b52a5c1d40571b52a309c9d85ae
jaws <jamei@deloitte.com.cn>
2021-04-25 11:14:36 +0800

    organize regular expression notes

---
+++ javascript/正则表达式/common.js
@@ -0,0 +1,14 @@
+// 去左空格;
+function lTrim(str) {
+    return str.replace(/^\s*/g, "");
+}
+
+// 去右空格
+function rTrim(str) {
+    return str.replace(/\s*$/g, "");
+}
+
+// 去空格
+function trim(str) {
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
